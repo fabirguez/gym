7c8ec050dd6953d2abb146f6dd5887ba82d7617c
fabirguez <72743213+fabirguez@users.noreply.github.com>
2021-02-13 19:48:10 +0100

    creacion carpetas y config

---
+++ app/config/database.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Parámetros de configuración de la base de datos.
+ */
+define('DBDRIVER', 'mysql');
+define('DBHOST', 'localhost');
+define('DBNAME', 'gimnasio');
+define('DBUSER', 'root');
+define('DBPASS', '');
---
+++ app/config/global.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Parámetros de configuración globales
+ * Definidos como constantes define.
+ */
+define('CONTROLLERS_FOLDER', 'controllers/');
+define('MODELS_FOLDER', 'models/');
+define('VIEWS_FOLDER', 'views/');
+define('PHOTOS_FOLDER', 'assets/img/');
+
+define('DEFAULT_CONTROLLER', 'Index');
+define('DEFAULT_ACTION', 'index');
+
+define('URL', 'http://localhost/gimnasio/app/');
